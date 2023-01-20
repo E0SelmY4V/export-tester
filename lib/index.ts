56ae69b3bc98a3716f68fd14e4359d89b50e2c70
E0SelmY4V-PC <2507917746@qq.com>
2023-01-20 23:14:58 +0800

    Ver 1.0.2

--- lib/index.ts
+++ lib/index.ts
@@ -10,7 +10,7 @@ import path = require('path');
 
 /**
  * Test the compatibility of your code.
- * @version 1.0.1
+ * @version 1.0.2
  * @license GPL-3.0-or-later
  * @link https://github.com/E0SelmY4V/export-tester
  */
